37aab2239a2e50f3bdcddb44d2b9ce6ab3ebe9bf
YOTO-creater <79891396+YOTO-creater@users.noreply.github.com>
2024-09-11 19:20:11 +0800

    Update GuestProgramLauncherComponent.java

--- app/src/main/java/com/winlator/xenvironment/components/GuestProgramLauncherComponent.java
+++ app/src/main/java/com/winlator/xenvironment/components/GuestProgramLauncherComponent.java
@@ -122,7 +122,7 @@ public class GuestProgramLauncherComponent extends EnvironmentComponent {
         envVars.put("USER", ImageFs.USER);
         envVars.put("TMPDIR", "/tmp");
         envVars.put("LC_ALL", "en_US.utf8");
-        envVars.put("DISPLAY", ":0");
+        envVars.put("DISPLAY", ":13");
         envVars.put("PATH", imageFs.getWinePath()+"/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin");
         envVars.put("LD_LIBRARY_PATH", "/usr/lib/aarch64-linux-gnu:/usr/lib/arm-linux-gnueabihf");
         envVars.put("ANDROID_SYSVSHM_SERVER", UnixSocketConfig.SYSVSHM_SERVER_PATH);
