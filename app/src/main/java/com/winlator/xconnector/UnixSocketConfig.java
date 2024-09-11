e20e79bd52f35e85ad489fc6d1930c4327249f12
YOTO-creater <79891396+YOTO-creater@users.noreply.github.com>
2024-09-11 19:13:29 +0800

    Update UnixSocketConfig.java

--- app/src/main/java/com/winlator/xconnector/UnixSocketConfig.java
+++ app/src/main/java/com/winlator/xconnector/UnixSocketConfig.java
@@ -5,11 +5,11 @@ import com.winlator.core.FileUtils;
 import java.io.File;
 
 public class UnixSocketConfig {
-    public static final String SYSVSHM_SERVER_PATH = "/tmp/.sysvshm/SM0";
-    public static final String ALSA_SERVER_PATH = "/tmp/.sound/AS0";
-    public static final String PULSE_SERVER_PATH = "/tmp/.sound/PS0";
-    public static final String XSERVER_PATH = "/tmp/.X11-unix/X0";
-    public static final String VIRGL_SERVER_PATH = "/tmp/.virgl/V0";
+    public static final String SYSVSHM_SERVER_PATH = "/tmp/.sysvshm/SM13";
+    public static final String ALSA_SERVER_PATH = "/tmp/.sound/AS13";
+    public static final String PULSE_SERVER_PATH = "/tmp/.sound/PS13";
+    public static final String XSERVER_PATH = "/tmp/.X11-unix/X13";
+    public static final String VIRGL_SERVER_PATH = "/tmp/.virgl/V13";
     public final String path;
 
     private UnixSocketConfig(String path) {
